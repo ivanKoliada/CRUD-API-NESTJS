51c5434df0ccccb0df550c27488d93715a755c70
Ivan Koliada <ivanKoliada69@gmail.com>
2023-01-05 16:59:40 +0300

    feat: env port

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,16 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['block', 'block-like', 'cjs-export', 'class', 'export', 'import'],
+        next: '*',
+      },
+      { blankLine: 'any', prev: ['import'], next: ['import'] },
+    ],
   },
 };
